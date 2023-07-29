3cfa9e2769f659cb40a37af94df140a2b32cfcf2
Sheikh Hassan Majeed <mhassanmajeed@live.com>
2023-07-29 08:05:06 +0500

    CRUD done

---
+++ all-products.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>All Products</title>
+    <!-- Add Bootstrap CSS link -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">All Products</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Category</th>
+                    <th>Sub-Category</th>
+                    <th>Image</th>
+                    <th>Update</th>
+                    <th>Delete</th> <!-- New column for Delete button -->
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Database connection
+                $con = new mysqli('localhost', 'root', '', 'ypp');
+                if ($con->connect_error) {
+                    die('Connection failed: ' . $con->connect_error);
+                }
+
+                // Pagination settings
+                $products_per_page = 5;
+                $current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+                $offset = ($current_page - 1) * $products_per_page;
+
+                // Fetch products from the database with pagination
+                $query = "SELECT * FROM products LIMIT $offset, $products_per_page";
+                $result = $con->query($query);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        ?>
+                        <tr>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['price']; ?></td>
+                            <td><?php echo $row['category']; ?></td>
+                            <td><?php echo $row['filter']; ?></td>
+                            <td><img src="./assets/image/<?php echo $row['image']; ?>" alt="Product Image" width="50"></td>
+                            <td><a href="edit-product.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Update</a></td>
+                            <td>
+                                <!-- Delete button with form submission -->
+                                <form method="post" action="delete-product.php" onsubmit="return confirm('Are you sure you want to delete this product?')">
+                                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                } else {
+                    ?>
+                    <tr>
+                        <td colspan="8">No products found.</td>
+                    </tr>
+                    <?php
+                }
+
+                $con->close();
+                ?>
+            </tbody>
+        </table>
+
+        <!-- Pagination links -->
+        <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+                <?php
+                // Calculate the total number of pages
+                $con = new mysqli('localhost', 'root', '', 'ypp');
+                $query = "SELECT COUNT(*) AS total FROM products";
+                $result = $con->query($query);
+                $total_products = $result->fetch_assoc()['total'];
+                $total_pages = ceil($total_products / $products_per_page);
+
+                // Display pagination links
+                for ($i = 1; $i <= $total_pages; $i++) {
+                    $active_class = ($current_page === $i) ? 'active' : '';
+                    echo "<li class='page-item $active_class'><a class='page-link' href='all-products.php?page=$i'>$i</a></li>";
+                }
+
+                $con->close();
+                ?>
+            </ul>
+        </nav>
+    </div>
+
+    <!-- Add Bootstrap JS and jQuery scripts at the end of the body -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ delete-product.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the product ID is provided
+    if (isset($_POST['id'])) {
+        $product_id = $_POST['id'];
+
+        // Database connection
+        $con = new mysqli('localhost', 'root', '', 'ypp');
+        if ($con->connect_error) {
+            die('Connection failed: ' . $con->connect_error);
+        }
+
+        // Prepare and execute the DELETE query
+        $query = "DELETE FROM products WHERE id=?";
+        $stmt = $con->prepare($query);
+        $stmt->bind_param("i", $product_id);
+
+        if ($stmt->execute()) {
+            // Product deleted successfully
+            echo '<script>alert("Product deleted successfully.");</script>';
+        } else {
+            // Failed to delete product
+            echo '<script>alert("Failed to delete product.");</script>';
+        }
+
+        $stmt->close();
+        $con->close();
+    } else {
+        // Product ID not provided, handle the error or redirect to index.php
+        echo '<script>alert("Product ID not provided.");</script>';
+    }
+}
+
+// Redirect back to index.php
+header('Location: all-products.php');
+exit();
+?>
