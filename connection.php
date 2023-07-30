fd11002304c46da305856d6680d476c1b0d27acd
Sheikh Hassan Majeed <mhassanmajeed@live.com>
2023-07-30 05:07:37 +0500

    default timezone

--- connection.php
+++ connection.php
@@ -1,5 +1,7 @@
 
 <?php
+//Set Default Timezone
+date_default_timezone_set("Asia/Karachi");
 // Step 1: Database connection
 $con = new mysqli('localhost', 'root', '', 'ypp');
 if ($con->connect_error) {
