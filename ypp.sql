1b8f26bcc4b5f802acac960a95edc01920f9c79c
Sheikh Hassan Majeed <mhassanmajeed@live.com>
2023-08-01 04:35:27 +0500

    database updated

--- ypp.sql
+++ ypp.sql
@@ -4,24 +4,13 @@
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `contact`
 --
 
 DROP TABLE IF EXISTS `contact`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `contact` (
   `id` int NOT NULL AUTO_INCREMENT,
   `F_Name` varchar(255) NOT NULL,
@@ -32,24 +21,42 @@ CREATE TABLE `contact` (
   `Num` varchar(255) NOT NULL,
   `Time_Date` varchar(255) NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
 
 --
 -- Dumping data for table `contact`
 --
 
-/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
-INSERT INTO `contact` VALUES (1,'Sheikh Hassan','Majeed','mhassanmajeed@live.com','Male','Hello','1234','2023-07-29 11:54:09pm');
-/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+--
+-- Table structure for table `general_settings`
+--
+
+DROP TABLE IF EXISTS `general_settings`;
+CREATE TABLE `general_settings` (
+  `logo` varchar(1000) NOT NULL,
+  `title` varchar(500) NOT NULL,
+  `subtitle` varchar(1000) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `address` varchar(1000) NOT NULL,
+  `number` varchar(500) NOT NULL,
+  `timings` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Dumping data for table `general_settings`
+--
+
+INSERT INTO `general_settings` VALUES ('logo.png','Yummy Pet Palate','A PET LOVER PARADISE','yummypetpalate@gmail.com','Karachi, Pakistan','92-312918743','12:00am to 12:00pm');
+
 
 --
 -- Table structure for table `products`
 --
 
 DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `name` varchar(255) DEFAULT NULL,
@@ -58,28 +65,42 @@ CREATE TABLE `products` (
   `filter` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4:
+
 
 --
 -- Dumping data for table `products`
 --
 
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (22,'Whiskas','2200','cat','food','catfood1.png'),(23,'Me-O','1800','cat','food','catfood8.png'),(24,'Josera','4000','cat','food','catfood3.png'),(25,'Bowl','2200','cat','accessory','bowl.png'),(26,'Collar','800','cat','accessory','collar.png'),(27,'House','4000','cat','accessory','house2.png'),(28,'Gim Cat','1000','cat','medicine','med1.png'),(29,'Quantum','1200','cat','medicine','med2.png'),(30,'Tape Tabs','900','cat','medicine','med3.png'),(31,'Ultra','2200','dog','food','food01.png'),(32,'Pedigree','1800','dog','food','food2.png'),(33,'Hypro','4000','dog','food','food3.png'),(34,'Bag','2200','dog','accessory','acc1.png'),(35,'Cage','4000','dog','accessory','acc2.png'),(36,'Leash','700','dog','accessory','acc3.png'),(37,'Bravecto','1000','dog','medicine','dmed1.png'),(38,'Vomitol','1200','dog','medicine','dmed02.png'),(39,'Digyton','900','dog','medicine','dmed3.png'),(41,'Hassan','0','dog','medicine','IMG_6192-modified.png');
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+
+INSERT INTO `products` VALUES (22,'Whiskas','2200','cat','food','catfood1.png'),(23,'Me-O','1800','cat','food','catfood8.png'),(24,'Josera','4000','cat','food','catfood3.png'),(25,'Bowl','2200','cat','accessory','bowl.png'),(26,'Collar','800','cat','accessory','collar.png'),(27,'House','4000','cat','accessory','house2.png'),(28,'Gim Cat','1000','cat','medicine','med1.png'),(29,'Quantum','1200','cat','medicine','med2.png'),(30,'Tape Tabs','900','cat','medicine','med3.png'),(31,'Ultra','2200','dog','food','food01.png'),(32,'Pedigree','1800','dog','food','food2.png'),(33,'Hypro','4000','dog','food','food3.png'),(34,'Bag','2200','dog','accessory','acc1.png'),(35,'Cage','4000','dog','accessory','acc2.png'),(36,'Leash','700','dog','accessory','acc3.png'),(37,'Bravecto','1000','dog','medicine','dmed1.png'),(38,'Vomitol','1200','dog','medicine','dmed02.png'),(39,'Digyton','900','dog','medicine','dmed3.png');
+
+
+--
+-- Table structure for table `social_links`
+--
+
+DROP TABLE IF EXISTS `social_links`;
+
+CREATE TABLE `social_links` (
+  `insta` varchar(500) DEFAULT NULL,
+  `twitter` varchar(500) DEFAULT NULL,
+  `linkedin` varchar(500) DEFAULT NULL,
+  `google` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Dumping data for table `social_links`
+--
+
+
+INSERT INTO `social_links` VALUES ('https://insta.com','https://twitter.com','https://linkedin.com','https://google.com');
+
 
 --
 -- Dumping routines for database 'ypp'
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-30  5:10:01
+-- Dump completed on 2023-08-01  4:30:37
